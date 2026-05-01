22ef4c92d9d0d097faead471f7ae169840522f35
ozgurkarakoyun <148861633+ozgurkarakoyun@users.noreply.github.com>
2026-05-01 23:26:54 +0300

    Change start.sh to use Python subprocess for uvicorn

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-exec uvicorn main:app --host 0.0.0.0 --port $PORT
+python -c "
+import os, subprocess, sys
+port = os.environ.get('PORT', '8080')
+subprocess.run(['uvicorn', 'main:app', '--host', '0.0.0.0', '--port', port])
+"
